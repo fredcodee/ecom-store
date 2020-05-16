21e5be3aac91188f32b388fd10772b28b0dfa076
fredcode <fredcode23@gmail.com>
2020-05-17 02:28:53 +0800

    add products to database

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, redirect, render_template, request, flash, jsonify, url_for, abort
-#from app.models import User, Favourites
-from app import db
+from app.models import Product,Order
+from app import db, photos
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
 from flask_wtf.file import FileField, FileAllowed
@@ -17,11 +17,26 @@ class AddProduct(FlaskForm):
   image = FileField('image', validators=[
                     FileAllowed(IMAGES, 'only images accepted.')])
 
+#routes
 @main.route("/", methods=['POST', 'GET'])
 def home():
   pass
 
-@main.route('/admin/add')
+
+@main.route('/admin')
+def admin():
+  return(render_template('admin/index.html', admin = True))
+
+
+@main.route('/admin/add', methods=["GET","POST"])
 def add():
   form = AddProduct()
+  if form.validate_on_submit():
+    image_url = photos.url(photos.save(form.image.data))
+    new_product = Product(name=form.name.data,
+                          stock=form.stock.data, price=form.price.data,description=form.description.data,image=image_url )
+    db.session.add(new_product)
+    db.session.commit()
+    return(redirect(url_for('main.admin')))
+
   return(render_template('admin/add-product.html', admin=True, form= form))
