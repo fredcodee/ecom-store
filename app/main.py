84ea2dd9e134836ba3d636b43952a344e2c9e7f7
fredcode <fredcode23@gmail.com>
2020-05-27 16:33:14 +0800

    cart session added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, Blueprint, redirect, render_template, request, flash, url_for
+from flask import Flask, Blueprint, redirect, render_template, request, flash, url_for,session
 from app.models import Product, Order
 from app import db, photos, create_app
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
+from wtforms import StringField, IntegerField, TextAreaField, HiddenField
 from flask_wtf.file import FileField, FileAllowed
 from flask_uploads import IMAGES
 
@@ -12,10 +12,15 @@ main = Blueprint('main', __name__)
 class AddProduct(FlaskForm):
   name = StringField('Name')
   price = IntegerField('Price')
-  stock = IntegerField('Price')
+  stock = IntegerField('Stock')
   description = TextAreaField('Description')
   image = FileField('image', validators=[
                     FileAllowed(IMAGES, 'only images accepted.')])
+
+#cart
+class AddToCart(FlaskForm):
+  quantity = IntegerField('Quantity')
+  id = HiddenField('ID')
 
 #routes
 @main.route("/")
@@ -24,17 +29,51 @@ def home():
 
   return(render_template('index.html', products=products))
 
-
+#view product page
 @main.route('/product/<idd>')
 def product(idd):
-  pass
+  product=Product.query.get(int(idd))
+  form = AddToCart()
+
+  return (render_template('view-product.html', product=product, form=form))
+
+#carts
+@main.route('/quick-add/<id>')
+def quick_add(id):
+  if 'cart' not in session:
+    session['cart'] = []
+
+  session['cart'].append({'id': id, 'quantity': 1})
+  session.modified = True
+
+  return(redirect(url_for('main.home')))
+
+@main.route('/add-to-cart', methods=['POST'])
+def add_to_cart():
+  if 'cart' not in session:
+    session['cart'] = []
+
+  form = AddToCart()
+
+  if form.validate_on_submit():
+
+    session['cart'].append({'id': form.id.data, 'quantity': form.quantity.data})
+    session.modified = True
+
+  return (redirect(url_for('main.home')))
+
+@main.route('/cart/<id>')
+def cart(id):
+  print(session['cart'])
+  return(render_template('cart.html'))
 
 
-@main.route('/cart')
-def cart():
-  pass
+@main.route('/checkout')
+def checkout():
+    return (render_template('checkout.html'))
 
 
+#Admin route
 @main.route('/admin/home')
 def admin():
   products = Product.query.all()
@@ -43,7 +82,7 @@ def admin():
 
   return(render_template('admin/index.html', admin=True, products=products, products_in_stock=products_in_stock, products_out_stock=products_out_stock))
 
-
+#add products to inventory
 @main.route('/admin/add', methods=["GET","POST"])
 def add():
   form = AddProduct()
